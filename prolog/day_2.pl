5730233f1e2704be8d2b6f2ef36a4b83738740ac
Ernest Walzel <ewalzel@gmail.com>
2017-10-02 15:59:59 +0200

    Prolog: Day 2 min() and sort()

--- prolog/day_2.pl
+++ prolog/day_2.pl
@@ -1,2 +1,10 @@
 reverse_list([A|[]],[A]).
 reverse_list([Head|Tail], RevList) :- reverse_list(Tail, ReversedTail), append(ReversedTail, [Head], RevList).
+
+min([A, B], A) :- A =< B.
+min([A, B], B) :- A > B.
+min([Head|Tail], Min) :- min(Tail, TailMin), min([Head, TailMin], Min).
+
+sort_list([A], [A]).
+sort_list([Head|Tail], Sorted) :- min([Head|Tail], Min), Head > Min, append(Tail, [Head], HeadAfterTail), sort_list(HeadAfterTail, Sorted).
+sort_list([Head|Tail], [Head|SortedTail]) :- min([Head|Tail], Head), sort_list(Tail, SortedTail), append([Head], SortedTail, Sorted).
